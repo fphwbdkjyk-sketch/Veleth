b5a9e8e41df0f54a3ce521570c0d921c0d97208f
fphwbdkjyk-sketch <fphwbdkjyk@privaterelay.appleid.com>
2026-03-13 07:06:39 -0600

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VelethApp from '../VelethApp_v6'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VelethApp />
+  </React.StrictMode>
+)
